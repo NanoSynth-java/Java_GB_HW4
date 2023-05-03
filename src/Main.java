c5a32c43e9523c8805a69e1986d0d30a0f7d6b8e
NanoSynth-java <comm2k3@gmail.com>
2023-05-03 21:18:47 +0200

    All methods done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Deque;
 import java.util.LinkedList;
 
 class Main
@@ -5,14 +6,28 @@ class Main
     public static void enqueue(LinkedList<Integer> value, int number) {
         value.add(number);
     }
+
+    public static LinkedList<Integer> dequeue(LinkedList<Integer> value) {
+        value.pollFirst();
+        return value;
+    }
+
+    public static LinkedList<Integer> first(LinkedList<Integer> value) {
+
+        value.getFirst();
+        return value;
+    }
     public static void main(String[] args)
     {
         LinkedList<Integer> list = new LinkedList<>();
         list.add(1);
         list.add(2);
         list.add(3);
-        System.out.println(list);;
+        System.out.println("Добавили в LinkedList 3 элемента: " + "\n" + list);
+        System.out.println("Пример работы метода enqueue: ");
         enqueue(list,4);
         System.out.println(list);
+        System.out.println("Пример работы метода dequeue(): " + "\n" + dequeue(list));
+        System.out.println("Пример работы метода first(): " + "\n" + first(list));
     }
 }
